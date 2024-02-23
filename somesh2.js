4a1e766101220a7ca6c937a446c4ff13f6338bde
MSSomesh <someshassom007@gmail.com>
2024-02-23 10:37:15 +0530

    Firstweb

---
+++ somesh2.js
@@ -0,0 +1,27 @@
+// let username="somesh";
+// console.log(username);
+// let gf= 5;
+// if(gf>3) {
+//     console.log("You are Play");
+// }
+// else {
+//     console.log("No comments Simply waste");
+// }
+// let online = true;
+// if(online) {
+//     console.log("you are online");
+// }
+// else {
+//     console.log("you are offline");
+// }
+// for(i=0;i<=10;i++){
+//     console.log(i);
+    
+// }
+// let num=['somesh','vicky','vijay'];
+// num.forEach(print)
+// function print(element) {
+//     if(element=='somesh'){
+//         console.log(element)
+//     }
+// }
